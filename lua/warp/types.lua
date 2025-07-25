b4fae4e19443e3e715eee70d3b8e0570eb309bbc
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-07-25 19:06:02 +0800

    fix: make list items format configurable (#19)

--- lua/warp/types.lua
+++ lua/warp/types.lua
@@ -5,6 +5,7 @@ local M = {}
 ---@class Warp.Config
 ---@field root_markers? string[] The root markers to check, defaults to { ".git" } and fallback to cwd, set to {} to nil it
 ---@field root_detection_fn? fun(): string The function to detect the root, defaults to `require("warp.storage").find_project_root`
+---@field list_item_format_fn? fun(entry: Warp.ListItem, idx: number, is_active: boolean|nil): string The function to format the list items lines
 ---@field keymaps? Warp.Config.Keymaps The keymaps for actions
 ---@field float_opts? Warp.Config.FloatOpts The floating window options
 
